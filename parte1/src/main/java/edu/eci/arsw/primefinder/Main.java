825bc7c7b69f674c8dfe4c66233133f5f278423b
Santiago Fetecua <santiago.fetecua@mail.escuelaing.edu.co>
2022-08-18 09:17:49 -0500

    update

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,14 +1,18 @@
 package edu.eci.arsw.primefinder;
 
+import java.util.Timer;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
+
+		PrimeFinderThread pft = new PrimeFinderThread(0, 10000000);
+		PrimeFinderThread pft1 = new PrimeFinderThread(10000001, 20000000);
+		PrimeFinderThread pft2 = new PrimeFinderThread(20000001, 30000000);
 		pft.start();
-		
-		
+		pft1.start();
+		pft2.start();
+
 	}
-	
+
 }
